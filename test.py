73719c03ff89fc0d6ee3969040a2fae71b1fdf73
pjvds <pj@born2code.net>
2013-06-06 11:29:56 +0200

    Print Redis server info

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import redis
 import os
 
 r = redis.StrictRedis(host=os.getenv("WERCKER_REDIS_HOST"), port=6379, db=0)
+print r.info()
+
 r.set('foo', 'bar')
 result = r.get('foo')
 assert result == 'bar'
