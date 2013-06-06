39671f7ad6ca29e121d22c21965f4ae04bfa6190
pjvds <pj@born2code.net>
2013-06-06 13:46:21 +0300

    Fix typo

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 host = os.getenv("WERCKER_REDIS_HOST")
 port = os.getenv("WERCKER_REDIS_PORT")
 
-r = redis.StrictRedis(host=, port=port, db=0)
+r = redis.StrictRedis(host=host, port=port, db=0)
 print r.info()
 
 r.set('foo', 'bar')
